fec348509620dda2bb9bbeb92cc9237189187584
Rodrigo Souza <rodrigo.c.souza@Hotmail.com>
2013-05-29 11:23:06 -0300

    First Commit

---
+++ TCPTrace/TCPTrace/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TCPTrace.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Porta a ser levantada:<br />
+        <asp:TextBox ID="txbPortaEscuta" runat="server"></asp:TextBox>
+        <br />
+        Servidor de destino:<br />
+        <asp:TextBox ID="txbServidor" runat="server"></asp:TextBox>
+        <br />
+        Porta de destino:<br />
+        <asp:TextBox ID="txbServidorPorta" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="btnIniciar" runat="server" onclick="btnIniciar_Click" 
+            Text="Iniciar" />
+&nbsp;<asp:Button ID="btnParar" runat="server" onclick="btnParar_Click" Text="Parar" />
+        <br />
+        <asp:Label ID="lblMsg" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ TCPTrace/TCPTrace/Default.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Diagnostics;
+using System.Configuration;
+
+namespace TCPTrace
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnIniciar_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.UseShellExecute = false;
+            psi.FileName = ConfigurationManager.AppSettings["LocalTCPTrace"].ToString();// @"C:\DEV\Programas\tcpTrace081\tcptrace.exe";
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+            psi.UseShellExecute = false;
+            psi.Arguments = @" /listen " + txbPortaEscuta.Text + @" /serverPort " + txbServidorPorta.Text + @" /serverName " + txbServidor.Text;
+            try
+            {
+                using (Process exeProcess = Process.Start(psi))
+                {
+                    lblMsg.Text = "Túnel aberto com sucesso";
+                }
+            }
+            catch (Exception exx)
+            {
+                lblMsg.Text = "Ocorreu um erro ao tentar abrir o túnel: " + exx.Message;
+            }
+        }
+
+        protected void btnParar_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.UseShellExecute = false;
+            psi.FileName = ConfigurationManager.AppSettings["LocalTCPTrace"].ToString();//@"C:\DEV\Programas\tcpTrace081\tcptrace.exe";
+            psi.WindowStyle = ProcessWindowStyle.Hidden;
+            psi.UseShellExecute = false;
+            psi.Arguments = @" /kill ";
+
+            try
+            {
+                using (Process exeProcess = Process.Start(psi))
+                {
+                    lblMsg.Text = "Todos os túneis foram fechados!";
+                }
+            }
+            catch(Exception exx)
+            {
+                lblMsg.Text = "Ocorreu um erro ao tentar fechar os túneis: " + exx.Message;
+            }
+        }
+    }
+}
